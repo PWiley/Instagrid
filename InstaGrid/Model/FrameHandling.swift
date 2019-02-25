03953f488bd2973494141905ff38e242120bed19
Patrick Wiley <you@example.com>
2019-02-25 10:24:16 +0100

    create FrameHandling

---
+++ InstaGrid/Model/FrameHandling.swift
@@ -0,0 +1,19 @@
+//
+//  FrameHandling.swift
+//  InstaGrid
+//
+//  Created by Patrick Wiley on 25.02.19.
+//  Copyright © 2019 PawilIOS. All rights reserved.
+//
+
+import Foundation
+
+
+
+
+
+enum frameState {
+    
+    case twoTwo, largeTwo, twoLarge
+    
+}
