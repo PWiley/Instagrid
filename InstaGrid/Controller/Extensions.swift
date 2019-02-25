00c8eff186eeb37b7fcddf729f036593122e527a
Patrick Wiley <you@example.com>
2019-02-25 10:25:43 +0100

    add extension file for UIView

---
+++ InstaGrid/Controller/Extensions.swift
@@ -0,0 +1,30 @@
+//
+//  Extensions.swift
+//  InstaGrid
+//
+//  Created by Patrick Wiley on 25.02.19.
+//  Copyright © 2019 PawilIOS. All rights reserved.
+//
+
+
+import UIKit
+
+
+extension UIView {
+    
+    func fadeIn(_ duration: TimeInterval = 0.5, delay: TimeInterval = 0.0, completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in}) {
+        self.alpha = 0
+        UIView.animate(withDuration: duration, delay: delay, options: UIView.AnimationOptions.curveEaseIn, animations: {
+            self.alpha = 1.0
+        }, completion: completion)  }
+    
+    // Using a function since `var image` might conflict with an existing variable
+    // (like on `UIImageView`)
+    func asImage() -> UIImage {
+        let renderer = UIGraphicsImageRenderer(bounds: bounds)
+        return renderer.image { rendererContext in
+            layer.render(in: rendererContext.cgContext)
+        }
+    }
+}
+
