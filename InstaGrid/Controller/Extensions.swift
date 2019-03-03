32b29f41c7e7a3639752a848c7a6218a033c0f9c
Patrick Wiley <you@example.com>
2019-03-03 18:20:04 +0100

    refresh comments

--- InstaGrid/Controller/Extensions.swift
+++ InstaGrid/Controller/Extensions.swift
@@ -11,6 +11,7 @@ import UIKit
 
 
 extension UIView {
+    
     // method creates a animation fading
     func fadeIn(_ duration: TimeInterval = 0.5, delay: TimeInterval = 0.0, completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in}) {
         self.alpha = 0
@@ -18,9 +19,9 @@ extension UIView {
             self.alpha = 1.0
         }, completion: completion)  }
     
-    
+    // Create an image renderer
     func asImage() -> UIImage {
-        let renderer = UIGraphicsImageRenderer(bounds: bounds) // Create an image renderer
+        let renderer = UIGraphicsImageRenderer(bounds: bounds)
         return renderer.image { rendererContext in
             layer.render(in: rendererContext.cgContext)
         }
