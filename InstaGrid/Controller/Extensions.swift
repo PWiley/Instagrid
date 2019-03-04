d4db3bae015323e9ea5c3eeba93539a5e9e196bb
Patrick Wiley <you@example.com>
2019-03-04 12:39:39 +0100

    refresh of extensions add comments

--- InstaGrid/Controller/Extensions.swift
+++ InstaGrid/Controller/Extensions.swift
@@ -12,12 +12,6 @@ import UIKit
 
 extension UIView {
     
-    // method creates a animation fading
-    func fadeIn(_ duration: TimeInterval = 0.5, delay: TimeInterval = 0.0, completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in}) {
-        self.alpha = 0
-        UIView.animate(withDuration: duration, delay: delay, options: UIView.AnimationOptions.curveEaseIn, animations: {
-            self.alpha = 1.0
-        }, completion: completion)  }
     
     // Create an image renderer
     func asImage() -> UIImage {
@@ -27,11 +21,23 @@ extension UIView {
         }
     }
     // MARK: Animation functions
+    
+    // method creates an animation fading for viewGeneral view
+    func fadeIn(_ duration: TimeInterval = 0.5, delay: TimeInterval = 0.0, completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in}) {
+        self.alpha = 0
+        UIView.animate(withDuration: duration, delay: delay, options: UIView.AnimationOptions.curveEaseIn, animations: {
+            self.alpha = 1.0
+        }, completion: completion)  }
+    
+    // method creates an animation to the up direction
+    
     func viewGeneralSwipeUp() {
         UIView.animate(withDuration: 0.5, animations: {
             self.transform = CGAffineTransform(translationX: 0, y: -self.frame.size.height * 2) }, completion : nil)
         
     }
+    
+    // method creates an animation to the left direction
     
     func viewGeneralSwipeLeft() {
         UIView.animate(withDuration: 0.5, animations: {
